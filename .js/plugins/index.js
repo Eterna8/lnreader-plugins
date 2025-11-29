03e843dbc1947b789be6747c01ab391653c4faa9
Eterna8 <joseyerica763@gmail.com>
2025-11-28 20:08:14 -0600

    build: rebuild plugin manifest and compiled sources

--- .js/plugins/index.js
+++ .js/plugins/index.js
@@ -5,5 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var BelleRepository_1 = __importDefault(require("@plugins/english/BelleRepository"));
 var ReadHive_1 = __importDefault(require("@plugins/english/ReadHive"));
-var plugins = [BelleRepository_1.default, ReadHive_1.default];
-exports.default = plugins;
+var PLUGINS = [BelleRepository_1.default, ReadHive_1.default];
+exports.default = PLUGINS;
